b5cb63a43f5815b872af8675380e57f984d885eb
RobertoGajardoR <robertogajardo.r@gmail.com>
2016-07-01 12:07:33 -0400

    conexionbd
    cambio cadena de conexion

--- datos/connect_db.php
+++ datos/connect_db.php
@@ -1,9 +1,9 @@
 <?php 
 	
-define('DB_SERVER','localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_DATABASE', 'inmobiliaria');
+define('DB_SERVER','us-cdbr-azure-central-a.cloudapp.net');
+define('DB_USERNAME', 'ba556d6229cd7c');
+define('DB_PASSWORD', 'e1818ca7');
+define('DB_DATABASE', 'acsm_4b4514f9f7969d8');
 $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 
 ?>
